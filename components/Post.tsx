d28ceb732ffcd360e465efe24a8bb4cd1d8735c6
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-03 20:12:28 +0330

    header

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,9 +1,9 @@
 
-export const Post: React.FC = ({largeTitle,subtitle,children}) => {
+export const Post: React.FC<{title:string, subtitle:string}> = ({title,subtitle,children}) => {
 return (<a href="#" className="block p-6 bg-white rounded-lg hover:bg-slate-100 dark:bg-midnightish dark:hover:bg-lightnight h-full">
-<h5 className="mb-2 text-2xl font-bold tracking-tight text-slate-900 dark:text-white">{props.largeTitle}</h5>
-<p className="font-normal text-muted">{props.subtitle}</p>
-<p className="font-normal text-neutral-800 dark:text-neutral-100">{props.children}</p>
+<h5 className="mb-2 text-2xl font-bold tracking-tight text-slate-900 dark:text-white">{title}</h5>
+<p className="font-normal text-muted">{subtitle}</p>
+<p className="font-normal text-neutral-800 dark:text-neutral-100">{children}</p>
 </a>
 )
 }
