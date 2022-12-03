adb22341a26c421152352ff5d7fe199b7c94c12a
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-03 21:00:09 +0330

    main page

--- components/TopSection.tsx
+++ components/TopSection.tsx
@@ -7,7 +7,7 @@ const TopSection:React.FC = () => {
     <div className="w-2/6 h-52 h-auto mr-4 dark:border-none border border-grayish rounded-lg">
     <Post title="About Us" subtitle="Know more about us">As you can see the paragraphs gracefully wrap around the floated image.</Post></div>
     <div className="w-4/6 h-52 h-auto dark:border-none border border-grayish rounded-lg">
-    <Post largeTitle="About Us" subtitle="Know more about us">As you can see the paragraphs gracefully wrap around the floated image.  Now imagine how this would look with some actual content in here, rather than just this boring placeholder text that goes on and on, but actually conveys no tangible information at. It simply takes up space and should not really be read.</Post>
+    <Post title="About Us" subtitle="Know more about us">As you can see the paragraphs gracefully wrap around the floated image.  Now imagine how this would look with some actual content in here, rather than just this boring placeholder text that goes on and on, but actually conveys no tangible information at. It simply takes up space and should not really be read.</Post>
     </div>
     </div>
     }
