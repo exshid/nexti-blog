80072a35306d41b225aa470e67df5abae0f5efdb
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-03 20:48:03 +0330

    search

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
-module.exports = nextConfig;
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./utils/cache')
+    }
+
+    return config
+  },
+  reactStrictMode: true,
+  images: {
+    domains: ['static.wixstatic.com'],
+},
+experimental: {
+  images: {
+      allowFutureImage: true
+  }},
+}
