8102d5af41dc68f622cae75cb87dda63530f4c4e
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-03 22:52:12 +0330

    post page

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -16,6 +16,12 @@ interface ContextProps extends ParsedUrlQuery {
   slug: string;
 }
 
+type Tag = {
+  post?: { // 👈️ could be undefined
+    tags?: string;
+  };
+};
+
 interface PostProps {
   frontMatter: MDXFrontMatter;
   mdx: any;
@@ -81,7 +87,7 @@ const Post: NextPage<PostProps> = ({ frontMatter, posts, mdx, previous, next }) 
       {posts.map((post,index) => (
       <div className="w-3/12 h-auto rounded-lg"> 
          <PostSidebar author={post.author} date={post.date}>
-          {post.tags.map((tag, index) => {
+          {post?.tags.map((tag, index) => {
                     return (
                       <li className="inline-block mx-1 mr-1" key={index}>
                         <Tag href={`/posts/tagged/${slugify(tag)}`}>{tag}</Tag>
