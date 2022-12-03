397e4dc031fd58517459aebeee0e890c68dd9bf9
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-03 21:52:19 +0330

    main page

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -3,7 +3,7 @@ import { formatDate } from "@/lib/formatDate";
 import type { MDXFrontMatter } from "@/lib/types";
 import { Prose } from "@/components/Prose";
 import { cx, slugify } from "@/lib/utils";
-import { Tag } from "./Tag";
+import { Tag } from "@/components/Tag";
 
 interface PostListProps {
   posts: Array<MDXFrontMatter>;
@@ -12,38 +12,37 @@ interface PostListProps {
 export const PostList: React.FC<PostListProps> = ({ posts }) => {
   return (
     <ul
-      className={cx(
-        "divide-y -my-8",
-        "divide-gray-200",
-        "dark:divide-gray-700"
-      )}
+      className="w-full py-8"
     >
-      {posts.map((post, index) => {
-        return (
-          <li className="py-8" key={index}>
-            <article>
-              <time
-                className={cx(
-                  "block mb-2",
-                  "text-gray-500",
-                  "dark:text-gray-400"
-                )}
-              >
-                {formatDate(post.date)}
-              </time>
-              <h2 className="font-bold text-xl">
-                <Link href={`/posts/${post.slug}`}>
-                  <a>{post.title}</a>
-                </Link>
-              </h2>
-              {post.description ? (
-                <div className="mt-3">
-                  <Prose>
-                    <p>{post.description}</p>
-                  </Prose>
-                </div>
-              ) : null}
-              {post.tags ? (
+<ul className="w-full py-8">
+          {posts.map((post) => (
+            <li
+              key={post.filePath}
+              className="px-8 bg-white dark:bg-midnightish hover:bg-daylight dark:hover:bg-lightnight transition"
+            >
+                              <Link href={`/posts/${post.slug}`}>
+                <a className="py-2 flex focus:outline-none focus:ring-4">
+	
+                {post.thumbnail && (<div className="overflow-hidden mr-4 rounded-lg relative w-2/6">
+                    <Image className="bg-contain w-full relative post-image" src={post.thumbnail}
+                    alt={post.title}
+                    />
+                    </div>
+                  )}
+<div class="w-4/6">
+                  {post.date && (
+                    <p className="uppercase mb-3 opacity-60">
+                      {post.date}
+                    </p>
+                  )}
+                  <h2 className="text-2xl md:text-3xl">{post.title}</h2>
+                  {post.description && (
+                    <p className="mt-3 text-lg overflow-hidden" style={{maxHeight:'50px'}}>
+                      {post.description}
+                    </p>
+                  )}
+               
+{post.tags ? (
                 <ul className="mt-4 flex flex-wrap space-x-2">
                   {post.tags.map((tag, index) => {
                     return (
@@ -54,10 +53,12 @@ export const PostList: React.FC<PostListProps> = ({ posts }) => {
                   })}
                 </ul>
               ) : null}
-            </article>
-          </li>
-        );
-      })}
+              </div>
+                  </a>
+                </Link>      
+            </li>
+          ))}
+        </ul>
     </ul>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,8 @@ interface HomeProps {
 const Home: NextPage<HomeProps> = ({ posts }) => {
   return (
     <Layout>
-        <div className="flex mt-3">
-	<div className="w-9/12 mr-4 h-auto dark:bg-midnightish rounded-lg dark:border-none border border-grayish">
+        <div className="flex mt-3 w-full">
+	<div className="w-9/12 mr-4 h-auto dark:bg-midnightish rounded-lg dark:border-none border border-grayish h-fit">
 
         <PostList posts={posts} />
       
