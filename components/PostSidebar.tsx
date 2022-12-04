92f8a36f8b6ed8719d531956c0d057a5f09e4955
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-04 20:32:36 +0330

    author

--- components/PostSidebar.tsx
+++ components/PostSidebar.tsx
@@ -14,7 +14,7 @@ return <ul style={{top: "85px"}} className="w-full dark:text-white dark:border-n
           "bg-reddish hover:bg-darker-reddish transition text-white"
         )}
       >
-        {slugify(author)}
+        {slugify(author ? author : '')}
       </a>
         
         {author}
