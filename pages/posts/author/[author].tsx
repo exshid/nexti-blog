5617302be22829065e770afcdbfa73ec177fa3a6
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-04 20:11:57 +0330

    author

---
+++ pages/posts/author/[author].tsx
@@ -0,0 +1,57 @@
+import type { GetStaticProps, GetStaticPaths, NextPage } from "next";
+import { ParsedUrlQuery } from "querystring";
+import { getAllMdx } from "@/lib/mdx";
+import { slugify } from "@/lib/utils";
+import { MDXFrontMatter } from "@/lib/types";
+import { TagTitle } from "@/components/TagTitle";
+import { PostList } from "@/components/PostList";
+
+interface ContextProps extends ParsedUrlQuery {
+  tag: string;
+}
+
+interface PostsProps {
+  author: string;
+  posts: Array<MDXFrontMatter>;
+}
+
+const Posts: NextPage<PostsProps> = ({ author, posts }) => {
+  return (
+    <>
+      <TagTitle title={`Posts written by ${author}`}>
+        <PostList posts={posts} />
+      </TagTitle>
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const mdxFiles = getAllMdx().map((post) => post["frontMatter"]);
+  return {
+    paths: Array.from(new Set(mdxFiles.map((file) => file.author).flat())).map(
+      (author) => {
+        return {
+          params: {
+            author: slugify(author!),
+          },
+        };
+      }
+    ),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { author } = context.params as ContextProps;
+  const mdxFiles = getAllMdx().map((post) => post["frontMatter"]);
+  return {
+    props: {
+      author,
+      posts: mdxFiles.filter((file) => {
+        return file.author?.includes(author);
+      }),
+    },
+  };
+};
+
+export default Posts;
