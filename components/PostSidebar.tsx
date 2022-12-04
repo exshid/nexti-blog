f987cce66a92857c18870bb60c80691475ea825f
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-04 20:28:46 +0330

    author

--- components/PostSidebar.tsx
+++ components/PostSidebar.tsx
@@ -1,10 +1,24 @@
 import {Tag} from '@/components/Tag';
 import { cx, slugify } from "@/lib/utils";
 
-const PostSidebar:React.FC<{author?:string, date:string}> = ({author,date,children}) => {
+      
+
+const PostSidebar:React.FC<{author?:string, date:string,href: string;}> = ({author,date,children, href}) => {
 return <ul style={{top: "85px"}} className="w-full dark:text-white dark:border-none border border-grayish flex flex-col justify-between content-start bg-white rounded-lg dark:text-white dark:bg-midnightish px-6 h-fit top-0 sticky">
         <li className="py-6">
-<p className="text-xl uppercase text-center mb-2">{author}</p>
+<p className="text-xl uppercase text-center mb-2">
+<Link href={`/posts/tagged/${slugify(href)}`}>
+      <a
+        className={cx(
+          "inline-block text-sm py-0.5 prose dark:prose-invert px-2.5 rounded-full hover:underline",
+          "bg-reddish hover:bg-darker-reddish transition text-white"
+        )}
+      >
+        {slugify(author)}
+      </a>
+        
+        {author}
+    </Link></p>
 <hr/>
         </li>
 
