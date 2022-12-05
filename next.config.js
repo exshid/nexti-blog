b98aa37c4a9affd25028766665694df2c1bfd1cd
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-05 19:36:14 +0330

    tags

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  webpack: (config, { isServer }) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.yml$/,
+          type: "json",
+          use: "yaml-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: "@svgr/webpack",
+        },
+      ]
+    );
+  
     if (isServer) {
       require('./utils/cache')
     }
+
 
     return config
   },
