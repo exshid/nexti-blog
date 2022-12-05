a9cec30c51d905e8b097e3b76670194523407e40
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-05 21:53:07 +0330

    date

--- components/TopSection.tsx
+++ components/TopSection.tsx
@@ -11,7 +11,7 @@ interface PostListProps {
 const TopSection:React.FC<PostListProps> = ({ posts }) => {
 
     return <div className="flex mb-4">
-                  {posts.slice(0, 2).filter(post => post === post.tags).filter(tag => tag === 'news').map((post,index) => (
+                  {posts.slice(0, 2).filter(tag => tag === 'news').filter(post => post === post.tags).map((post,index) => (
          <div key={index} className="w-2/6 h-52 h-auto mr-4 dark:border-none border border-grayish rounded-lg">
     <Post title="post.title" subtitle="post.date">{post.description}</Post>
     </div>
