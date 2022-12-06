b0bbcfdc4a749c508ca87f16d804948c03719982
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-07 00:19:28 +0330

    top section

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,5 @@
  const Post: React.FC<{title:string, subtitle:string}> = ({title,subtitle,children}) => {
-return (<a href="#" className="block p-6 bg-white rounded-lg hover:bg-slate-100 dark:bg-midnightish dark:hover:bg-lightnight h-full">
+return (<a href="#" className="block p-6 bg-white w-full rounded-lg hover:bg-slate-100 dark:bg-midnightish dark:hover:bg-lightnight h-full">
 <h5 className="mb-2 text-2xl font-bold tracking-tight text-slate-900 dark:text-white">{title}</h5>
 <p className="font-normal text-muted">{subtitle}</p>
 <p className="font-normal text-neutral-800 dark:text-neutral-100">{children}</p>
--- components/TopSection.tsx
+++ components/TopSection.tsx
@@ -12,11 +12,11 @@ interface PostListProps {
   export const TopSection: React.FC<PostListProps> = ({ posts }) => {
       return <div className="flex mb-4">
                {posts.filter((file) => file.tags?.includes('typescript')).map((post,index) => (
-                              <Link key={index} href={`/posts/${post.slug}`}>
-                              <a className="py-2 flex focus:outline-none focus:ring-4">
+                              <Link key={index} href={`/posts/${post.slug}`} className="odd:w-2/6 even:w-4/6">
+                              <a className="py-2 flex w-full focus:outline-none focus:ring-4">
               
     <div 
-    className="odd:w-2/6 even:w-4/6 h-52 h-auto odd:mr-4 dark:border-none border border-grayish rounded-lg">
+    className="w-full h-52 h-auto odd:mr-4 dark:border-none border border-grayish rounded-lg">
     <Post title={post.title} subtitle={formatDate(post.date)}>{post.description}</Post></div>
     </a>
     </Link>
