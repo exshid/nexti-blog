ac3e5166c026901933cbcba7667c7fbcfe428e8f
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-07 00:15:33 +0330

    top section

--- components/TopSection.tsx
+++ components/TopSection.tsx
@@ -12,10 +12,10 @@ interface PostListProps {
   export const TopSection: React.FC<PostListProps> = ({ posts }) => {
       return <div className="flex mb-4">
                {posts.filter((file) => file.tags?.includes('typescript')).map((post,index) => (
-                              <Link href={`/posts/${post.slug}`}>
+                              <Link key={index} href={`/posts/${post.slug}`}>
                               <a className="py-2 flex focus:outline-none focus:ring-4">
               
-    <div key={index}
+    <div 
     className="odd:w-2/6 even:w-4/6 h-52 h-auto odd:mr-4 dark:border-none border border-grayish rounded-lg">
     <Post title={post.title} subtitle={formatDate(post.date)}>{post.description}</Post></div>
     </a>
