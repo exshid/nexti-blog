bdb42321ba106016728c551e6ca7781bfe561783
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-06 21:22:42 +0330

    tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ interface PostListProps {
 
 export const Header: React.FC<PostListProps> = ({ posts }) => {
   const { pathname } = useRouter();
-console.log(posts.filter((file) => file.tags?.includes('news')))
+console.log(posts.filter((file) => file.tags?.includes('typescript')))
 console.log(posts.filter((file) => file.tags?.includes('react')))
 
 return (
