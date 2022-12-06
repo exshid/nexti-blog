580a1243f3c1e0bf5ab60ac6417842c21a65a805
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-06 21:31:06 +0330

    tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ function buttonHandler(){
 }
   return (
     <Layout>
-      <Header posts={posts} />
+      <Header posts={posts.slice(0, postNum)} />
           <TopSection/>
           <Topics tags={tags}/>
         <div className="flex mt-3 w-full ">
