fc1cac35ec6a44ec67b6a59266897c490c21d1e6
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-06 21:29:19 +0330

    tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -62,7 +62,7 @@ return (
   <div id="mega-menu-full-dropdown" className="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-midnightish dark:border-gray-600 hidden">
         <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
         <ul>
-            {posts.slice(0, 3).filter((file) => file.tags?.includes('typescript')).map((post,index) => (
+            {posts.filter((file) => file.tags?.includes('typescript')).map((post,index) => (
 
                 <li key={index}>
                                     <Link href={`/posts/${post.slug}`}>
@@ -75,7 +75,7 @@ return (
              ))}
              </ul>
             <ul>
-            {posts.slice(0, 3).filter((file) => file.tags?.includes('react')).map((post,index) => (
+            {posts.filter((file) => file.tags?.includes('react')).map((post,index) => (
 
                 <li key={index}>
                                     <Link href={`/posts/${post.slug}`}>
