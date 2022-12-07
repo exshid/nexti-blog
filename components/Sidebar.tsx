a040022657e4ea1d2b14b30a2bb92bcb05654415
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-07 23:18:19 +0330

    sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,7 +9,7 @@ interface PostListProps {
   }
   export const Sidebar: React.FC<PostListProps> = ({ posts }) => {
 
-    return <ul style={{top: "85px"}} className="w-full dark:text-white dark:border-none border border-grayish flex flex-col justify-between content-start bg-white rounded-lg dark:text-white dark:bg-midnightish px-6 h-fit top-0 sticky">
+    return <ul style={{top: "85px"}} className="w-full divide-slate-800 divide-y dark:text-white dark:border-none border border-grayish flex flex-col justify-between content-start bg-white rounded-lg dark:text-white dark:bg-midnightish px-6 h-fit top-0 sticky">
                   {posts.filter((file) => file.tags?.includes('typescript')).map((post,index) => (
     <Link key={index} href={`/posts/${post.slug}`}>
                               <a className="py-2 flex focus:outline-none focus:ring-4">       
