a49c617899fd15c01e6fd8f60004a07b85f41e61
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-08 01:21:20 +0330

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -84,15 +84,25 @@ return (
             <ul>
             {posts.filter((file) => file.tags?.includes('react')).map((post,index) => (
 
-                <li key={index}>
-                                    <Link href={`/posts/${post.slug}`}>
-                    <a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-lightnight">
-                        <div className="font-semibold">{post.title}</div>
-                        <span className="text-sm font-light text-gray-500 dark:text-gray-400">{post.description}</span>
-                    </a>
-                </Link>
-                </li>
-             ))}
+
+<Link key={index} href={`/posts/${post.slug}`}>
+<a className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-lightnight">
+<li className="flex my-1" >
+{post.thumbnail && (<div className="overflow-hidden mr-4 rounded-lg relative w-1/5 image-span">
+<Image className="bg-contain w-full relative header-image" src={post.thumbnail} height= "100px"
+alt={post.title}
+/>
+</div>
+)}
+
+<div className="w-4/5">
+
+    <div className="font-semibold">{post.title}</div>
+    <span className="text-sm font-light text-gray-500 dark:text-gray-400">{post.description}</span>
+</div>
+</li>
+</a>
+</Link>             ))}
              </ul>
         </div>
     </div>
