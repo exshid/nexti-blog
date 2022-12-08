ecc2b818e07a60306a7839befceae0ed6b85e071
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-08 23:40:19 +0330

    tag

--- components/Topics.tsx
+++ components/Topics.tsx
@@ -6,9 +6,7 @@ import { TagContent } from "@/lib/tags";
 type Props = {
     tags: TagContent[];
   };
-  
-
-    const Topics: React.FC<Props> = ({tags}) => {
+      const Topics: React.FC<Props> = ({tags}) => {
     return (<div className="flex mb-4 w-full">
     <div className="w-full h-auto">
     <ul className="flex justify-around flex-wrap px-3 pb-3 bg-white rounded-lg dark:border-none border border-grayish dark:bg-midnightish h-auto">
