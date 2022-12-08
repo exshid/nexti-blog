ad7b1ed40508d1bc4f18bd4c23419884a4884349
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-08 06:49:06 +0330

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,71 @@ export const Header: React.FC<PostListProps> = ({ posts }) => {
 return (
 
     <header className="mt-3 px-4 z-40	sticky top-0 w-full">
-
+<Navbar
+  fluid={true}
+  rounded={true}
+>
+  <Navbar.Brand href="https://flowbite.com/">
+    <img
+      src="https://flowbite.com/docs/images/logo.svg"
+      className="mr-3 h-6 sm:h-9"
+      alt="Flowbite Logo"
+    />
+    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      Flowbite
+    </span>
+  </Navbar.Brand>
+  <div className="flex md:order-2">
+    <Dropdown
+      arrowIcon={false}
+      inline={true}
+      label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true}/>}
+    >
+      <Dropdown.Header>
+        <span className="block text-sm">
+          Bonnie Green
+        </span>
+        <span className="block truncate text-sm font-medium">
+          name@flowbite.com
+        </span>
+      </Dropdown.Header>
+      <Dropdown.Item>
+        Dashboard
+      </Dropdown.Item>
+      <Dropdown.Item>
+        Settings
+      </Dropdown.Item>
+      <Dropdown.Item>
+        Earnings
+      </Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item>
+        Sign out
+      </Dropdown.Item>
+    </Dropdown>
+    <Navbar.Toggle />
+  </div>
+  <Navbar.Collapse>
+    <Navbar.Link
+      href="/navbars"
+      active={true}
+    >
+      Home
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      About
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Services
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Pricing
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Contact
+    </Navbar.Link>
+  </Navbar.Collapse>
+</Navbar>
 
 <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-midnightish w-full z-20 top-0 left-0 rounded-lg border border-grayish dark:border-none">
   <div className="container flex flex-wrap items-center justify-between mx-auto">
