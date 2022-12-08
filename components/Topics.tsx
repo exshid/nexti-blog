7779a6e5bff916fc2f6f4e9658556b37ef26fea5
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-08 23:27:09 +0330

    tag

--- components/Topics.tsx
+++ components/Topics.tsx
@@ -11,9 +11,9 @@ type Props = {
     const Topics: React.FC<Props> = ({tags}) => {
     return (<div className="flex mb-4 w-full">
     <div className="w-full h-auto">
-    <ul className="flex justify-around flex-wrap px-3 bg-white rounded-lg dark:border-none border border-grayish dark:bg-midnightish h-auto">
+    <ul className="flex justify-around flex-wrap px-3 pb-3 bg-white rounded-lg dark:border-none border border-grayish dark:bg-midnightish h-auto">
     {tags.map((it, i) => (
-<li key={i} style={{width: '32.5%'}} className="rounded-lg my-3 dark:bg-evening bg-daylight dark:hover:bg-lightnight dark:text-white inline-block h-1/3 p-6 pt-4">            <TagLink tag={it} />
+<li key={i} style={{width: '32.5%'}} className="rounded-lg mt-3 dark:bg-evening bg-daylight dark:hover:bg-lightnight dark:text-white inline-block h-1/3 p-6 pt-4">            <TagLink tag={it} />
 </li>
 ))}
 </ul>
