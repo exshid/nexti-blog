aa8723d852bfdd02c9efc2cc384e2d581760ff24
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-08 22:53:40 +0330

    dropdown

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,7 @@ export const Header: React.FC<PostListProps> = ({ posts }) => {
   const [drowpdown, setDropdown] = useState()
   
 const dropdownHandler = () =>{
-      setDropdown(true)
+      setDropdown('true')
     
     console.log(dropdown)
 
@@ -61,7 +61,7 @@ return (
     </ul>
   </div>
   </div>
- {dropdown &&(<div id="mega-menu-full-dropdown" className='mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-midnightish dark:border-gray-600'>
+ {dropdown && <div id="mega-menu-full-dropdown" className='mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-midnightish dark:border-gray-600'>
         <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
         <ul>
             {posts.filter((file) => file.tags?.includes('typescript')).map((post,index) => (
@@ -101,7 +101,7 @@ return (
              </ul>
         </div>
     </div>
- )}
+ }
 </nav>
 
     </header>
