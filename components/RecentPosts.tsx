db8323a1e1c40d3a735190d9183be76543344280
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-09 23:46:56 +0330

    read more

--- components/RecentPosts.tsx
+++ components/RecentPosts.tsx
@@ -1,8 +1,8 @@
 const RecentPosts: React.FC<{title:string, subtitle?:string, author:string, background:string}> = ({title,subtitle,author,background}) => {
-return <figure style={{background: `linear-gradient(70deg, rgba(20, 20, 20, .8), rgba(20, 20, 20, 0)), url(${background}})`}} className="my-2 even:ml-2 rounded-lg odd:mr-2 flex flex-col items-center justify-center p-8 text-center bg-white dark:border-none border border-grayish hover:rounded-lg dark:bg-midnightish dark:hover:bg-lightnight transition hover:bg-daylight">
-        <blockquote className="max-w-2xl mx-auto text-gray-500 dark:text-gray-400">
-            <h3 className="inline-block bg-white dark:bg-midnightish hover:bg-daylight dark:hover:bg-lightnight hover:bg-darker-white dark:bg-midnightish hover:bg-daylight dark:hover:bg-lightnight transition rounded-lg text-lg font-semibold text-gray-900 dark:text-white p-2">{title}</h3>
-            <p className="my-4 font-light bg-white dark:bg-midnightish hover:bg-daylight dark:hover:bg-lightnight hover:bg-darker-white dark:bg-midnightish hover:bg-daylight dark:hover:bg-lightnight transition rounded-lg p-2 overflow-hidden" style={{maxHeight:'60px'}}>{subtitle}</p>
+return <figure style={{background: `linear-gradient(70deg, rgba(20, 20, 20, 1), rgba(20, 20, 20, 0)), url(${background}})`}} className="my-2 even:ml-2 rounded-lg odd:mr-2 flex flex-col items-center justify-center p-8 text-center dark:border-none border border-grayish transition">
+        <blockquote className="max-w-2xl mx-auto text-white">
+            <h3 className="inline-block  transition rounded-lg text-lg font-semibold text-white p-2">{title}</h3>
+            <p className="my-4 font-light  transition rounded-lg p-2 overflow-hidden" style={{maxHeight:'60px'}}>{subtitle}</p>
         </blockquote>
     </figure>
 }
