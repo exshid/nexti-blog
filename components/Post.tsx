b8b9afab64bca751b1534b3c7a52ac68f746f053
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-09 19:36:08 +0330

    tags

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,8 +1,8 @@
  const Post: React.FC<{title:string, subtitle:string}> = ({title,subtitle,children}) => {
-return (<a href="#" className="block p-6 bg-white w-full rounded-lg hover:bg-slate-100 dark:bg-midnightish dark:hover:bg-lightnight h-full">
-<h5 className="mb-2 text-2xl font-bold tracking-tight text-slate-900 dark:text-white">{title}</h5>
+return (<a href="#" className="block p-6 bg-white w-full rounded-lg hover:bg-daylight dark:bg-midnightish dark:hover:bg-lightnight h-full">
+<h5 className="mb-2 text-2xl font-bold tracking-tight text-midnightish dark:text-white">{title}</h5>
 <p className="font-normal text-muted">{subtitle}</p>
-<p className="font-normal text-neutral-800 dark:text-neutral-100">{children}</p>
+<p className="font-normal text-midnightish dark:text-white">{children}</p>
 </a>
 )
 }
