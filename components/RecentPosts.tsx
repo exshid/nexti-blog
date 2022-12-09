f9372a60c30b8c0ddc28b3606a9307acf6c904da
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-09 20:50:23 +0330

    read more

--- components/RecentPosts.tsx
+++ components/RecentPosts.tsx
@@ -1,4 +1,4 @@
-const RecentPosts: React.FC<{title:string, subtitle:string, author:string}> = ({title,subtitle,author}) => {
+const RecentPosts: React.FC<{title:string, subtitle?:string, author:string}> = ({title,subtitle,author}) => {
 return <div className="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2">
     <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
         <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
