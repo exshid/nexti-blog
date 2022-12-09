e445a627fd86c0c994eb55464d459fa25d9853a1
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-09 19:08:04 +0330

    tags

--- components/Topics.tsx
+++ components/Topics.tsx
@@ -9,10 +9,12 @@ type Props = {
     tags: TagContent[];
   };
       const Topics: React.FC<Props> = ({tags}) => {
+        console.log(tags)
     return (<div className="flex mb-4 w-full">
     <div className="w-full h-auto">
     <ul className="flex justify-around flex-wrap px-3 pb-3 bg-white rounded-lg dark:border-none border border-grayish dark:bg-midnightish h-auto">
     {tags.map((it, i) => (
+      console.log(it)
     <Link key={i} href={`/posts/tagged/${it.slug}`}>
       <a style={{width: '32.5%'}} className="inline-block h-1/3">
 <li className="flex justify-center rounded-lg mt-3 dark:bg-evening bg-daylight dark:hover:bg-lightnight dark:text-white inline-block h-1/3 p-6 pt-4"
