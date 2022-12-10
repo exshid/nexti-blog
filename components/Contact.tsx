4b2f11080dc6f5087a5e1deb7cc5be11b8d52042
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-10 18:43:35 +0330

    contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,5 @@
+//@ts-nocheck
 const Contact: React.FC = () => {
-//@ts-ignore
 return <div id="authentication-modal" tabIndex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
     <div className="relative w-full h-full max-w-md md:h-auto">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -19,7 +19,6 @@ return <div id="authentication-modal" tabIndex="-1" aria-hidden="true" className
 
                     <div>
                     <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-//@ts-ignore
 <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
  </div>
                  <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
