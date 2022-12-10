8458c80b2035e18acc37ea0fb49375b86186ac13
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-10 19:20:44 +0330

    contact form

--- components/Header.tsx
+++ components/Header.tsx
@@ -66,7 +66,7 @@ return (
         <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-lightnight dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
       </li>
       <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-lightnight dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" data-modal-toggle="staticModal" onClick={contactHandler}>Contact</a>
+        <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-lightnight dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" data-modal-toggle="staticModal" type="button" onClick={contactHandler}>Contact</a>
       </li>
      
     </ul>
