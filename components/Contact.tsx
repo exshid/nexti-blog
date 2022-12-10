7cada3e931db28c1f2e31c9608375c888d5215c3
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-10 18:38:03 +0330

    contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,5 @@
 const Contact: React.FC = () => {
-
+//@ts-ignore
 return <div id="authentication-modal" tabIndex="-1" ariaHidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
     <div class="relative w-full h-full max-w-md md:h-auto">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
