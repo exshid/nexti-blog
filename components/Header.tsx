8818a327d743d0a4c970985c72be764e3dd90299
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-10 19:14:01 +0330

    contact form

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,7 +25,6 @@ const dropdownHandler = () =>{
 
 const contactHandler = () =>{
     
-  setContactUs(!contactUs)
 console.log(contactUs)   
 }
 
@@ -67,13 +66,13 @@ return (
         <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-lightnight dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
       </li>
       <li>
-        <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-lightnight dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" onClick={contactHandler}>Contact</a>
+        <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-lightnight dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" data-modal-toggle="staticModal" onClick={contactHandler}>Contact</a>
       </li>
      
     </ul>
   </div>
   </div>
-  {contactUs && <Contact/> }
+  <Contact/>
  {dropdown && <div id="mega-menu-full-dropdown" className='mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-midnightish dark:border-gray-600'>
         <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
         <ul onClick={dropdownCloseHandler}>
