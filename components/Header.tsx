10db967cb0a32d8c31c9b437f65709a9f9a9c2aa
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-11 20:04:59 +0330

    search

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,7 @@ import { Prose } from "@/components/Prose";
 import { cx, slugify } from "@/lib/utils";
 import Contact from "@/components/Contact";
 import Search from "@/components/Search";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
 
 
 interface PostListProps {
@@ -76,6 +77,9 @@ return (
         <span className="sr-only">Open search</span>
         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
     </button>
+  </div>
+  <div>
+    <ThemeSwitcher/>
   </div>
   <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
     <ul className="flex flex-col p-4 mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:bg-white dark:bg-midnightish md:dark:bg-midnightish">
