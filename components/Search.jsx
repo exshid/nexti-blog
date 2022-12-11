fa4f3cace36a222adc2fc8fe6e20cb3636f55d4e
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-11 19:41:44 +0330

    search

--- components/Search.jsx
+++ components/Search.jsx
@@ -42,16 +42,7 @@ export default function Search() {
     ref={searchRef}>
     <div className="relative w-full max-w-7xl h-full md:h-auto">
         <div className="relative bg-white rounded-lg dark:bg-midnightish">
-            <div className="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
-                <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-                    Search
-                </h3>
-                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-lg p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
-                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>  
-                    <span className="sr-only">Close modal</span>
-                </button>
-            </div>
-            <div className="p-6 space-y-6">
+                        <div className="p-6 space-y-6">
       <input id="default-search" className="block text-lg w-full p-4 pl-10 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-midnightish dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={onChange}
         onFocus={onFocus}
         placeholder='Search posts'
