a34ac6e6eec0d8379767a214d191b49b493812f8
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-11 19:24:47 +0330

    search

--- components/Header.tsx
+++ components/Header.tsx
@@ -60,7 +60,7 @@ return (
 </Link>
   <div className="flex md:order-2">
   <div className="block space-y-4 md:flex md:space-y-0 md:space-x-4">
-    <button className="block w-full md:w-auto text-white bg-blue-700 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-midnightish dark:hover:bg-lightnight dark:focus:ring-lightnight" type="button" data-modal-toggle="extralarge-modal">
+    <button className="block w-full md:w-auto text-white bg-blue-700 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-midnightish dark:hover:bg-lightnight dark:focus:ring-lightnight" type="button">
     <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>    </button>
 </div>
 
