29d11af05e92528eb85a063bade679021024dccb
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-11 23:12:13 +0330

    posts

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -59,7 +59,7 @@ const Post: NextPage<PostProps> = ({ frontMatter, mdx, tags, posts, previous, ne
             {previous ? (
            
            <div className=" w-full lg:max-w-full lg:flex">
-           <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/mountain.jpg')" title="Mountain">
+           <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
            </div>
            <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
              <div className="mb-8">
