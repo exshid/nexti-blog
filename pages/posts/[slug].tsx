d84f0cb646cbbfb2f9d9ed6ab936b7302a7ee02f
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-11 23:11:08 +0330

    posts

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -58,24 +58,24 @@ const Post: NextPage<PostProps> = ({ frontMatter, mdx, tags, posts, previous, ne
           >
             {previous ? (
            
-           <div class=" w-full lg:max-w-full lg:flex">
-           <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/mountain.jpg')" title="Mountain">
+           <div className=" w-full lg:max-w-full lg:flex">
+           <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/mountain.jpg')" title="Mountain">
            </div>
-           <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-             <div class="mb-8">
-               <p class="text-sm text-gray-600 flex items-center">
+           <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+             <div className="mb-8">
+               <p className="text-sm text-gray-600 flex items-center">
                  
                  Previous               </p>
-               <div class="text-gray-900 font-bold text-xl mb-2">                <Link href={`/posts/${previous?.slug}`}>
+               <div className="text-gray-900 font-bold text-xl mb-2">                <Link href={`/posts/${previous?.slug}`}>
                   <a>{previous?.title}</a>
                 </Link></div>
-               <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+               <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
              </div>
-             <div class="flex items-center">
-               <img class="w-10 h-10 rounded-full mr-4" src="/ben.png" alt="Avatar of Writer"/>
-               <div class="text-sm">
-                 <p class="text-gray-900 leading-none">John Smith</p>
-                 <p class="text-gray-600">Aug 18</p>
+             <div className="flex items-center">
+               <img className="w-10 h-10 rounded-full mr-4" src="/ben.png" alt="Avatar of Writer"/>
+               <div className="text-sm">
+                 <p className="text-gray-900 leading-none">John Smith</p>
+                 <p className="text-gray-600">Aug 18</p>
                </div>
              </div>
            </div>
