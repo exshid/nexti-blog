e5b75887e0d15ad23db57b1e9e417e4bb2784600
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-18 20:29:50 +0330

    border

--- components/TopSection.tsx
+++ components/TopSection.tsx
@@ -10,10 +10,10 @@ interface PostListProps {
   }
   
   export const TopSection: React.FC<PostListProps> = ({ posts }) => {
-      return <div className="flex mb-2 w-full flex-col md:flex-row border-t border-reddish border-t-8 rounded-lg md:even:rounded-tr-lg md:odd:rounded-tl-lg md:rounded-none">
+      return <div className="flex mb-2 w-full flex-col md:flex-row">
                {posts.slice(0,2).filter((file) => file.tags?.includes('politics')).map((post,index) => (
                               <Link key={index} href={`/posts/${post.slug}`}>
-                              <a className="pt-1 py-2 flex odd:mr-4 focus:outline-none w-full md:odd:w-2/6 md:even:w-4/6 focus:ring-4">
+                              <a className="pt-1 py-2 flex odd:mr-4 focus:outline-none w-full border-t border-reddish border-t-8 rounded-lg md:even:rounded-tr-lg md:odd:rounded-tl-lg md:rounded-none md:odd:w-2/6 md:even:w-4/6 focus:ring-4">
               
     <div 
     className="w-full h-52 h-auto dark:border-none border border-grayish rounded-lg">
