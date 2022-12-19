eac4c53ceb3629c55718b0f9a6488bf972378b34
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-19 12:47:38 +0330

    form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -15,18 +15,18 @@ return <div className="relative w-full h-full flex justify-center md:h-auto">
             <form>
   <div className="mb-6">
     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your name</label>
-    <input placeholder="Your name" type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-lightnight dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+    <input placeholder="Your name" type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-darker-reddish focus:border-darker-reddish block w-full p-2.5 dark:bg-lightnight dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-darker-reddish dark:focus:border-darker-reddish" required/>
   </div>
   <div className="mb-6">
     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your email</label>
-    <input placeholder="Your email" type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-lightnight dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+    <input placeholder="Your email" type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-darker-reddish focus:border-darker-reddish block w-full p-2.5 dark:bg-lightnight dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-darker-reddish dark:focus:border-darker-reddish" required/>
   </div>
   <div className="mb-6">
 
   <label htmlFor="message" className="block mb-2 text-sm font-medium sr-only text-gray-900 dark:text-white">Your message</label>
-<textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-lightnight dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+<textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-darker-reddish focus:border-darker-reddish dark:bg-lightnight dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-darker-reddish dark:focus:border-darker-reddish" placeholder="Leave a comment..."></textarea>
 </div>
-   <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-reddish dark:hover:bg-darker-reddish dark:focus:ring-darker-reddish">Submit</button>
+   <button type="submit" className="text-white mb-6 bg-reddish hover:bg-darker-reddish focus:ring-4 focus:outline-none focus:ring-darker-reddish font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-reddish dark:hover:bg-darker-reddish dark:focus:ring-darker-reddish">Submit</button>
 </form>
     </div>
 </div>
