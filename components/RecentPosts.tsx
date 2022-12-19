015541cd6b36b38e615711b136f5f2f199d96964
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-19 11:27:32 +0330

    border

--- components/RecentPosts.tsx
+++ components/RecentPosts.tsx
@@ -12,7 +12,7 @@ alt='Post thumbnail'
 />
 <figure style={{position: 'absolute', height: '190px', background: 'linear-gradient(70deg, rgba(20, 20, 20, 1), rgba(20, 20, 20, 0))'}} className="transition group recent-posts rounded-lg flex flex-col items-center justify-center px-8 text-center dark:border-none border border-grayish transition">
                 <blockquote className="max-w-2xl mx-auto text-left text-white">
-            <h3 className="inline-block group-hover:translate-x-1 transition font-bold transition rounded-lg text-xl sm:text-2xl md:text-3xl text-white p-2">{title}</h3>
+            <h3 className="inline-block group-hover:translate-x-1 transition font-bold transition rounded-lg text-xl sm:text-2xl lg:text-3xl text-white p-2">{title}</h3>
             <p className="font-light transition rounded-lg p-2 overflow-hidden" style={{maxHeight:'60px'}}>{subtitle}</p>
         </blockquote>
     </figure>
