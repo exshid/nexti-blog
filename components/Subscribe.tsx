bbbd6b8910c8bfb48a4d003b90dfe90f98c5cd06
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-19 12:59:51 +0330

    form

---
+++ components/Subscribe.tsx
@@ -0,0 +1,16 @@
+const Subscribe: React.FC = () => {
+
+return <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <form className="space-y-6" action="#">
+        <h3 className="text-xl font-medium text-gray-900 dark:text-white">Get more updates...</h3>
+        <div>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your email</label>
+            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required/>
+        </div>
+        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+    </form>
+</div>
+
+}
+
+export default Subscribe
