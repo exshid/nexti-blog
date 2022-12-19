0f2ca7e606f53e3a3f657c965b5544837f422ac4
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-19 13:08:10 +0330

    form

--- components/Subscribe.tsx
+++ components/Subscribe.tsx
@@ -1,13 +1,14 @@
 const Subscribe: React.FC = () => {
 
-return <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+return <div className="w-full mt-4 p-4 dark:bg-midnightish bg-white rounded-lg border border-grayish dark:border-none sm:p-6 md:p-8">
     <form className="space-y-6" action="#">
         <h3 className="text-xl font-medium text-gray-900 dark:text-white">Get more updates...</h3>
+        <div className="flex">
         <div>
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your email</label>
-            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required/>
+            <input type="email" name="email" id="email" className="bg-gray-50 border border-grayish dark:border-none text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-9/12 p-2.5 dark:bg-lightnight dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required/>
         </div>
-        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+        <button type="submit" className="w-3/12 text-white bg-reddish hover:bg-darker-reddish focus:ring-4 focus:outline-none focus:ring-darker-reddish font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login to your account</button></div>
     </form>
 </div>
 
