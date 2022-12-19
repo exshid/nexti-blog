3e92600da7f00c6dc4f7df367a6063091615dee4
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-19 14:50:55 +0330

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,13 @@
 import siteConfig from "@/data/siteConfig";
 import { cx } from "@/lib/utils";
 import { ThemeSelect } from "@/components/ThemeSelect";
+import siteConfig from "@/data/siteConfig";
 
 export const Footer: React.FC = () => {
   return (<footer className="w-full mb-3 px-4">
     <div className="border-b border-reddish border-b-8 rounded-lg">
  <div className="dark:bg-midnightish bg-white p-4 rounded-lg border border-grayish dark:border-none md:flex md:items-center md:justify-between md:p-6">
-    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 {siteConfig.siteName}. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
