f0461a6b176b978078b1729f4140278a45757b8e
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-20 18:07:07 +0330

    search

--- components/Search.jsx
+++ components/Search.jsx
@@ -49,17 +49,17 @@ export default function Search(props) {
         type='text'
         value={query}
       />
-      { results.length > 0 && (
+      { results.length <= 1 ? (
         <ul className="flex flex-wrap	w-full p-4 pl-10 text-gray-900 border border-grayish dark:border-none dark:bg-lightnight focus:ring-darker-reddish focus:border-darker-reddish block rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-midnightish dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          {results.length <= 1 ? results.slice(0,16).map(({ id, title }) => (
+          {results.slice(0,16).map(({ id, title }) => (
             <li className="p-2 transition hover:translate-x-1 w-full lg:w-1/2 text-lg" key={id}>
               <Link href="/posts/[id]" as={`/posts/${id}`}>
                 <a onClick={props.onSearch} className="hover:translate-x-1">{title}</a>
                 </Link>  
             </li>            
-          )) : (<p>Nothing found</p>)}
+          )) 
         </ul>
-      ) }
+      ) : (<p>Nothing found</p>)}
                   </div>
 
             </div>
