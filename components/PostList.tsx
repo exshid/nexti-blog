fb3a4284d8cdd3c752bffe98373ed0671ac6bae4
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-20 16:17:47 +0330

    description

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -43,7 +43,7 @@ export const PostList: React.FC<PostListProps> = ({ posts, className }) => {
                   )}
                   <h2 className="text-xl md:text-3xl prose dark:prose-invert">{post.title}</h2>
                   {post.description && (
-                    <p className="mt-3 md:text-lg overflow-hidden prose dark:prose-invert" style={{maxHeight:'50px'}}>
+                    <p className="mt-3 md:text-lg overflow-hidden hidden md:block prose dark:prose-invert" style={{maxHeight:'50px'}}>
                       {post.description}
                     </p>
                   )}
