4471cd24566dd55206329e54b1f66f132e529335
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-20 17:10:33 +0330

    search

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -1,5 +1,7 @@
 export const Prose: React.FC = ({ children }) => {
-  document.querySelector('.image-container-lead p:first-of-type').classList.add('lead')
+  let lead = document.querySelector('.image-container-lead p:first-of-type') as HTMLInputElement | null
+  
+  lead.classList.add('lead')
   return (
     <div className="max-w-none prose prose-a:text-pink-600 dark:prose-invert image-container image-container-lead">
       {children}
