aa3bf8a1f157b4a1ebb08d558c14f62665b28523
Alan Parniyan <101585281+exshid@users.noreply.github.com>
2022-12-26 21:08:56 +0330

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -131,7 +131,7 @@ return (
   
   }
 {contactUs && <Contact close={contactHandler}/>}
- {dropdown && <div id="mega-menu-full-dropdown" className='mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-midnightish dark:border-gray-600'>
+ {dropdown && <div id="mega-menu-full-dropdown" className='mt-1  shadow-sm bg-gray-50 md:bg-white dark:bg-evening border border-grayish dark:border-none dark:bg-midnightish absolute rounded-lg'>
         <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
         <ul onClick={dropdownCloseHandler}>
             {posts.filter((file) => file.tags?.includes('featured')).map((post,index) => (
